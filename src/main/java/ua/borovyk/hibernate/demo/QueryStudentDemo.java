6d64006c51eaef2c4c3558fe721c596e51f743ed
oborovyk <pennerall@gmail.com>
2021-12-13 15:56:22 +0200

    QueryStudentDemo.java created

--- src/main/java/ua/borovyk/hibernate/demo/QueryStudentDemo.java
+++ src/main/java/ua/borovyk/hibernate/demo/QueryStudentDemo.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ua.borovyk.hibernate.demo.entity.Student;
 
+import java.util.List;
+
 public class QueryStudentDemo {
     public static void main(String[] args) {
 
@@ -14,19 +16,36 @@ public class QueryStudentDemo {
         Session session = factory.getCurrentSession();
 
         try {
-            System.out.println("Creating a new Student object");
-            Student tmpStudent = new Student("Petro", "Petrov", "petrov@gmail.com");
-
             session.beginTransaction();
 
-            System.out.println("Saving the student...");
-            session.save(tmpStudent);
+            List<Student> theStudents = session.createQuery("from Student").getResultList();
+
+            displayStudents(theStudents);
+
+            theStudents = session.createQuery("from Student s where s.lastName='Duck'").getResultList();
+
+            System.out.println("\n\nLast name Duck");
+            displayStudents(theStudents);
+
+            System.out.println("\n\nLast name Scott or First name Nick");
+            theStudents = session.createQuery("from Student s where s.lastName='Scott' OR s.firstName='Nick'").getResultList();
+            displayStudents(theStudents);
+
+            System.out.println("\n\nEmail contains duck");
+            theStudents = session.createQuery("from Student s where s.email LIKE 'duck%'").getResultList();
+            displayStudents(theStudents);
 
             session.getTransaction().commit();
 
             System.out.println("Done!");
         } finally {
             factory.close();
+        }
+    }
+
+    private static void displayStudents(List<Student> theStudents) {
+        for(Student student: theStudents) {
+            System.out.println(student);
         }
     }
 }
